b7e59809943f5e6e5523bb2d46f8afc7707ed3cc
Rachel Kennedy <rachel.kennedy13@nhs.net>
2025-06-17 16:08:01 +0100

    CCM-10064: Add NHS App Account Test (#20)

---
+++ tests/test_nhs_app_account_endpoint.py
@@ -0,0 +1,28 @@
+def test_nhsapp_account(api_client, url):
+    ods_code = "T00001"
+    pages = [1,2,3,4]
+    
+    for page in pages:
+        response = api_client.get_nhsapp_account(
+            {
+                "ods-organisation-code": ods_code,
+                "page": page
+            }
+        )
+
+        assert response.status_code == 200
+
+        resp = response.json()
+
+        assert resp.get("data").get("id") is not None
+        assert resp.get("data").get("id") == ods_code
+        assert resp.get("data").get("type") == "NhsAppAccounts"
+        assert resp.get("data").get("attributes").get("accounts") is not None
+        assert len(resp.get("data").get("attributes").get("accounts")) > 0
+        for i in range(len(resp.get("data").get("attributes").get("accounts"))):
+            assert resp.get("data").get("attributes").get("accounts")[i].get("NhsNumber") is not None
+            assert resp.get("data").get("attributes").get("accounts")[i].get("NhsNumber") != ""
+            assert resp.get("data").get("attributes").get("accounts")[i].get("NotificationsEnabled") is not None
+        assert resp.get("links").get("self").startswith(url)
+        assert resp.get("links").get("self") \
+            .endswith(f"/channels/nhsapp/accounts?ods-organisation-code={ods_code}&page={page}")
